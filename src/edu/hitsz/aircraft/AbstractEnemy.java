8f9298a0dd1252cc946ba2735f654194d7a8e513
Speculator <4498548@qq.com>
2026-04-02 18:07:52 +0800

    fix: 修复敌机超出窗口底部后不消失的问题
    当敌机纵向位置超出窗口高度时，调用vanish方法使其消失

--- src/edu/hitsz/aircraft/AbstractEnemy.java
+++ src/edu/hitsz/aircraft/AbstractEnemy.java
@@ -1,9 +1,18 @@
 package edu.hitsz.aircraft;
 
+import edu.hitsz.application.Main;
+
 public abstract class AbstractEnemy extends AbstractAircraft {
 
     public AbstractEnemy(int locationX, int locationY, int speedX, int speedY, int hp) {
         super(locationX, locationY, speedX, speedY, hp);
     }
-}
 
+    @Override
+    public void forward() {
+        super.forward();
+        if (locationY >= Main.WINDOW_HEIGHT) {
+            vanish();
+        }
+    }
+}
